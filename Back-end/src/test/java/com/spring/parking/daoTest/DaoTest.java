211e249dda9d42eb765c9c0a407d668f9536718d
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2023-12-28 16:32:09 +0100

    Change datasource to fix error of testFindAllParking test

--- Back-end/src/test/java/com/spring/parking/daoTest/DaoTest.java
+++ Back-end/src/test/java/com/spring/parking/daoTest/DaoTest.java
@@ -3,10 +3,8 @@ package com.spring.parking.daoTest;
 import com.spring.parking.dao.CarParkingInfoDao;
 import com.spring.parking.dao.ParkingDao;
 import com.spring.parking.dao.ParkingLotDao;
-import com.spring.parking.dto.CarParkingInfoDto;
 import com.spring.parking.dto.ParkingDto;
 import com.spring.parking.dto.ParkingLotDto;
-import com.spring.parking.entity.CarParkingInfo;
 import com.spring.parking.entity.Parking;
 import com.spring.parking.entity.ParkingLot;
 import com.spring.parking.mapper.ParkingLotMapper;
@@ -14,7 +12,6 @@ import com.spring.parking.mapper.ParkingMapper;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -45,20 +42,12 @@ public class DaoTest {
 
     @Test
     public void testSaveParking(){
-//        List<ParkingLotDto> parkingLotDtoList = new ArrayList<>();
-//        ParkingLotDto parkingLotDto1 = new ParkingLotDto();
-//        ParkingLotDto parkingLotDto2 = new ParkingLotDto();
-//        parkingLotDtoList.add(parkingLotDto1);
-//        parkingLotDtoList.add(parkingLotDto2);
-
         ParkingDto parkingDto = ParkingDto.builder()
                 .id(1)
                 .openTime("8:00 AM")
                 .closeTime("11:00 PM")
 //                .parkingLots(parkingLotDtoList)
                 .build();
-
-
         Parking parkingEntity = parkingMapper.toParkingEntity(parkingDto);
         Parking savedParking = parkingDao.save(parkingEntity);
         Assertions.assertThat(savedParking).isNotNull();
@@ -67,27 +56,18 @@ public class DaoTest {
 
     @Test
     public void testFindAllParking(){
-//        List<ParkingLotDto> parkingLotDtoList = new ArrayList<>();
-
-//        ParkingLotDto parkingLotDto1 = new ParkingLotDto();
-//        ParkingLotDto parkingLotDto2 = new ParkingLotDto();
-//        parkingLotDtoList.add(parkingLotDto1);
-//        parkingLotDtoList.add(parkingLotDto2);
 
         ParkingDto parking_dto_1 = ParkingDto.builder()
-                .id(1)
                 .openTime("8:00 AM")
                 .closeTime("11:00 PM")
 //                .parkingLots(parkingLotDtoList)
                 .build();
         ParkingDto parking_dto_2 = ParkingDto.builder()
-                .id(2)
                 .openTime("11:00 PM")
                 .closeTime("7:00 !M")
 //                .parkingLots(parkingLotDtoList)
                 .build();
 
-        //TODO : Cannot invoke toParkingEntity() because parkingMapper is null
         Parking parking_entity_1 = parkingMapper.toParkingEntity(parking_dto_1);
         Parking parking_entity_2 = parkingMapper.toParkingEntity(parking_dto_2);
 
