f05e510b8cfe97b72e97c7b57572cb07cbdb1490
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2023-12-31 18:26:15 +0100

    Fix error of not-null property error

--- Back-end/src/test/java/com/spring/parking/daoTest/ParkingLotDaoTest.java
+++ Back-end/src/test/java/com/spring/parking/daoTest/ParkingLotDaoTest.java
@@ -1,7 +1,9 @@
 package com.spring.parking.daoTest;
 
+import com.spring.parking.dao.ParkingDao;
 import com.spring.parking.dao.ParkingLotDao;
 import com.spring.parking.dto.ParkingLotDto;
+import com.spring.parking.entity.Parking;
 import com.spring.parking.entity.ParkingLot;
 import com.spring.parking.mapper.ParkingLotMapper;
 import org.assertj.core.api.Assertions;
@@ -18,39 +20,38 @@ public class ParkingLotDaoTest {
 
     @Autowired
     private ParkingLotDao parkingLotDao;
-
     @Autowired
     private ParkingLotMapper parkingLotMapper;
+    @Autowired
+    private ParkingDao parkingDao;
 
     @Test
     public void testToSaveParkingLot(){
-        // TODO : not-null property references a null or transient value : com.spring.parking.entity.ParkingLot.parking
+        Parking parkingEntity = new Parking();
+        Parking savedParking = parkingDao.save(parkingEntity);
         ParkingLotDto parkingLotDto = ParkingLotDto.builder()
                 .parkingLotNumber(1)
                 .carParkingInfoDto(null)
-                .parking(null)
+                .parking(savedParking)
                 .price(10.00)
                 .build();
         ParkingLot parkingLotEntity = parkingLotMapper.toParkingLotEntity(parkingLotDto);
-
         ParkingLot parkingLotSaved = parkingLotDao.save(parkingLotEntity);
-
         Assertions.assertThat(parkingLotSaved).isNotNull();
     }
 
     @Test
     public void testDeleteParkingLot(){
-        // TODO : not-null property references a null or transient value : com.spring.parking.entity.ParkingLot.parking
+        Parking parkingEntity = new Parking();
+        Parking savedParking = parkingDao.save(parkingEntity);
+
         ParkingLotDto parkingLotDto = ParkingLotDto.builder()
                 .parkingLotNumber(1)
+                .parking(savedParking)
                 .price(10.00)
                 .build();
-
-        // ! Fix error of toParkingLotEntity method
         ParkingLot parkingLotEntity = parkingLotMapper.toParkingLotEntity(parkingLotDto);
-
         parkingLotDao.save(parkingLotEntity);
-
         parkingLotDao.deleteById(parkingLotEntity.getParkingLotNumber());
         Optional<ParkingLot> parkingLotDeleted = parkingLotDao.findById(parkingLotEntity.getParkingLotNumber());
 
