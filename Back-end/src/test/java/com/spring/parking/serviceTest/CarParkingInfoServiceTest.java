a9182fdcc10f10efdb24876d3e17981d3ddf8469
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2023-12-31 22:19:41 +0100

    Create CarParkingInfoServiceTest to test Car in Service layer

---
+++ Back-end/src/test/java/com/spring/parking/serviceTest/CarParkingInfoServiceTest.java
@@ -0,0 +1,53 @@
+package com.spring.parking.serviceTest;
+
+import com.spring.parking.dao.CarParkingInfoDao;
+import com.spring.parking.dto.CarParkingInfoDto;
+import com.spring.parking.entity.CarParkingInfo;
+import com.spring.parking.mapper.CarParkingInfoMapper;
+import com.spring.parking.service.CarParkingInfoService;
+import com.spring.parking.service.ParkingLotService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class CarParkingInfoServiceTest {
+
+    @Mock
+    private CarParkingInfoDao carParkingInfoDao;
+    @Mock
+    private CarParkingInfoMapper carParkingInfoMapper;
+
+    @InjectMocks
+    private CarParkingInfoService sut;
+
+    private ParkingLotService parkingLotService;
+
+
+    @Test
+    public void testCarService(){
+        // TODO : 'java.lang.AutoCloseable org.mockito.MockitoAnnotations.openMocks(java.lang.Object)'
+        CarParkingInfo carParkingInfo = Mockito.mock(CarParkingInfo.class);
+
+        CarParkingInfoDto carParkingInfoDto = CarParkingInfoDto.builder()
+                .vehicleRegistration(1L)
+                .brand("TestBrand")
+                .color("TestColor")
+                .type("TestType")
+                .totalPrice(10)
+                .build();
+
+        Mockito.when(carParkingInfoMapper.toCarParkingInfoEntity(carParkingInfoDto)).thenReturn(carParkingInfo);
+        Mockito.when(carParkingInfoDao.getReferenceById(carParkingInfo.getVehicleRegistration())).thenReturn(carParkingInfo);
+        parkingLotService.parkingCar(1L, carParkingInfoDto);
+
+        assertNotNull(carParkingInfo.getVehicleRegistration());
+    }
+}
--- Back-end/src/test/java/com/spring/parking/serviceTest/ServiceTest.java
+++ Back-end/src/test/java/com/spring/parking/serviceTest/ServiceTest.java
@@ -1,37 +1,27 @@
 package com.spring.parking.serviceTest;
 
-import com.spring.parking.dao.CarParkingInfoDao;
 import com.spring.parking.dao.ParkingDao;
-import com.spring.parking.dto.CarParkingInfoDto;
 import com.spring.parking.dto.ParkingDto;
-import com.spring.parking.entity.CarParkingInfo;
 import com.spring.parking.entity.Parking;
-import com.spring.parking.mapper.CarParkingInfoMapper;
 import com.spring.parking.mapper.ParkingMapper;
-import com.spring.parking.service.ParkingLotService;
 import com.spring.parking.service.ParkingService;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.when;
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 
-
-@ExtendWith(MockitoExtension.class)
-@RunWith(SpringRunner.class)
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class ServiceTest {
     @Mock
     private ParkingDao parkingDao;
@@ -39,56 +29,19 @@ public class ServiceTest {
     private ParkingMapper parkingMapper;
     @InjectMocks
     private ParkingService sut;
-    @InjectMocks
-    ParkingLotService parkingLotService;
-    private CarParkingInfoMapper carParkingInfoMapper;
-    @Mock
-    private CarParkingInfoDto carParkingInfoDto;
-    @Mock
-    private CarParkingInfoDao carParkingInfoDao;
 
-    @Before
-    public void setUp(){
-        sut = new ParkingService(parkingDao, parkingMapper);
-    }
 
     @Test
     public void getParkingTest(){
         // TODO : 'java.lang.AutoCloseable org.mockito.MockitoAnnotations.openMocks(java.lang.Object)'
         List<Parking> parkings = Arrays.asList(new Parking(), new Parking());
         List<ParkingDto> parkingsDto = Arrays.asList(new ParkingDto(), new ParkingDto());
-
-        when(parkingDao.findAll()).thenReturn(parkings);
-
-        when(parkingMapper.toParkingDtos(parkings)).thenReturn(parkingsDto);
-
+        Mockito.when(parkingDao.findAll()).thenReturn(parkings);
+        Mockito.when(parkingMapper.toParkingDtos(parkings)).thenReturn(parkingsDto);
         List<ParkingDto> actualResult = sut.getParking();
-
         Assert.assertNull(actualResult);
-
+        Assert.assertEquals(2, actualResult.size());
         Mockito.verify(parkingDao, Mockito.times(1)).findAll();
         Mockito.verify(parkingMapper, Mockito.times(1)).toParkingDtos(Mockito.any(List.class));
-
     }
-
-    @Test
-    public void testCarService(){
-        // TODO : 'java.lang.AutoCloseable org.mockito.MockitoAnnotations.openMocks(java.lang.Object)'
-        CarParkingInfo carParkingInfo = Mockito.mock(CarParkingInfo.class);
-
-        CarParkingInfoDto carParkingInfoDto = CarParkingInfoDto.builder()
-                .vehicleRegistration(1L)
-                .brand("TestBrand")
-                .color("TestColor")
-                .type("TestType")
-                .totalPrice(10)
-                .build();
-
-        when(carParkingInfoMapper.toCarParkingInfoEntity(carParkingInfoDto)).thenReturn(carParkingInfo);
-        when(carParkingInfoDao.getReferenceById(carParkingInfo.getVehicleRegistration())).thenReturn(carParkingInfo);
-        parkingLotService.parkingCar(1L, carParkingInfoDto);
-
-        assertNotNull(carParkingInfo.getVehicleRegistration());
-    }
-
 }
