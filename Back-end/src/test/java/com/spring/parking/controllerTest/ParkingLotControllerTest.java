f4e4391a4ce61337b52e35bd9eadef905fccac9e
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-01-05 21:15:52 +0100

    Create testGetParkingLots in parking lot controller

--- Back-end/src/test/java/com/spring/parking/controllerTest/ParkingLotControllerTest.java
+++ Back-end/src/test/java/com/spring/parking/controllerTest/ParkingLotControllerTest.java
@@ -1,15 +1,33 @@
 package com.spring.parking.controllerTest;
 
 import com.spring.parking.controller.ParkingLotsController;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = ParkingLotsController.class)
 @AutoConfigureMockMvc(addFilters = false)
 @ExtendWith(MockitoExtension.class)
 public class ParkingLotControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetParkingLots() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/parking//parkingLots").accept(MediaType.APPLICATION_JSON))
+                .andDo(print()).andExpect(status().isOk());
+
+    }
 
 }
