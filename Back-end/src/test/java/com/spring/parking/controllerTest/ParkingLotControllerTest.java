a76bb960633ae864efd4dca26b26c0eae18b4ea0
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-01-08 12:57:23 +0100

    Add testUnparkingCar in ParkingLot controller

--- Back-end/src/test/java/com/spring/parking/controllerTest/ParkingLotControllerTest.java
+++ Back-end/src/test/java/com/spring/parking/controllerTest/ParkingLotControllerTest.java
@@ -1,6 +1,7 @@
 package com.spring.parking.controllerTest;
 
 import com.spring.parking.controller.ParkingLotsController;
+import com.spring.parking.service.ParkingLotService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -14,6 +15,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.doNothing;
 
 @WebMvcTest(controllers = ParkingLotsController.class)
 @AutoConfigureMockMvc(addFilters = false)
@@ -23,11 +25,21 @@ public class ParkingLotControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    private ParkingLotService parkingLotService;
+
     @Test
     public void testGetParkingLots() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/parking//parkingLots").accept(MediaType.APPLICATION_JSON))
+        // TODO : java.lang.IllegalStateException: Failed to load ApplicationContext
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/parking/parkingLots").accept(MediaType.APPLICATION_JSON))
                 .andDo(print()).andExpect(status().isOk());
 
     }
 
+    @Test
+    public void testUnparkingCar() throws Exception {
+        // TODO : java.lang.IllegalStateException: Failed to load ApplicationContext
+        doNothing().when(parkingLotService).unparkingCar(1L,null);
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/parking/unparkingCar/{parkingLotNumber}").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
 }
