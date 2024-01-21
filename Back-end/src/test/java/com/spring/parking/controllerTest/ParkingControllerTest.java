eb0280332e3aff4d485bb63f917b1799d7954512
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-01-21 13:50:19 +0100

    Refactor ParkingControllerTest

--- Back-end/src/test/java/com/spring/parking/controllerTest/ParkingControllerTest.java
+++ Back-end/src/test/java/com/spring/parking/controllerTest/ParkingControllerTest.java
@@ -8,6 +8,7 @@ import com.spring.parking.service.ParkingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,31 +18,30 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
 import java.util.Arrays;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @WebMvcTest(controllers = ParkingController.class)
 @AutoConfigureMockMvc(addFilters = false)
 @ExtendWith(MockitoExtension.class)
 public class ParkingControllerTest {
-    // TODO : Refactor this code
-
     @Autowired
     private MockMvc mockMvc;
-
     @MockBean
     private ParkingService parkingService;
 
     @Mock
-    private com.spring.parking.entity.Parking parking;
-
-
     private ParkingDto parkingDto;
     @Autowired
     private ObjectMapper objectMapper;
+
+    @InjectMocks
+    private ParkingController parkingController;
 
     @BeforeEach
     public void init() {
@@ -56,13 +56,20 @@ public class ParkingControllerTest {
 
     @Test
     public void testGetParkingThatReturnParkingList() throws Exception {
+        List<ParkingDto> parkingDtoList = Arrays.asList(
+                new ParkingDto(),
+                new ParkingDto()
+        );
+        when(parkingService.getParking()).thenReturn(parkingDtoList);
+        List<ParkingDto> result = parkingController.getParking();
+        verify(parkingService, times(1)).getParking();
+        assertEquals(result.size(),parkingDtoList.size());
         mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/parking").accept(MediaType.APPLICATION_JSON))
                 .andDo(print()).andExpect(status().isOk());
     }
 
     @Test
     public void createParkingInitThatReturnCreated() throws Exception {
-
         mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/parking/init")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(parkingDto)).accept(MediaType.APPLICATION_JSON))
--- Back-end/src/test/java/com/spring/parking/serviceTest/CarParkingInfoServiceTest.java
+++ Back-end/src/test/java/com/spring/parking/serviceTest/CarParkingInfoServiceTest.java
@@ -1,23 +1,16 @@
 package com.spring.parking.serviceTest;
 
 import com.spring.parking.dao.CarParkingInfoDao;
-
 import com.spring.parking.entity.CarParkingInfo;
-
 import com.spring.parking.service.CarParkingInfoService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.mockito.Mockito;
-
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
